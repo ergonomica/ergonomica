44ed8654b05fe76d59480af59ff469f8f83a96af
Liam Schumm <liamschumm@icloud.com>
2016-11-14 16:59:24 -0600

    draft of autocomplete

---
+++ completer.py
@@ -0,0 +1,12 @@
+from verbs import verbs
+
+def completer(text, state):
+    options = [i for i in verbs.verbs if i.startswith(text)]
+    #print options
+    #try:
+    if state > 2:
+        return None
+    if options != []:# len(options):
+        return options[0]
+    else:
+        return None
