fad54fc8fe64dd63575b87fcee3c4aea4b260ddb
Liam Schumm <liamschumm@icloud.com>
2016-12-10 11:43:40 -0600

    Fix 'python' function

--- lib/lib/python.py
+++ lib/lib/python.py
@@ -10,20 +10,30 @@
 Defines the "python" command.
 """
 
+import sys
+import code
+
 verbs = {}
 
 def python(env, args, kwargs):
     """@Drop into a python REPL."""
-    temp_space = {}
-    try:
-        temp_space = globals()
-        temp_space.update({"exit":sys.exit})
-        temp_space.update({"quit":sys.exit})
-        temp_space.update(env.namespace)
-        code.InteractiveConsole(locals=temp_space).interact()
-    except SystemExit:
-        for key in temp_space:
-            env.namespace[key] = temp_space[key]
-        return ""
+    temp_space = globals()
+    if args != []:
+        # for some reason exec is a statement
+        for arg in args:
+            exec arg in temp_space
+    else:
+        try:
+            temp_space = globals()
+            temp_space.update({"exit":sys.exit})
+            temp_space.update({"quit":sys.exit})
+            temp_space.update(env.namespace)
+            code.InteractiveConsole(locals=temp_space).interact()
+        except SystemExit:
+            pass
+    
+    for key in temp_space:
+        env.namespace[key] = temp_space[key]
+    return ""
 
 verbs["python"] = python
