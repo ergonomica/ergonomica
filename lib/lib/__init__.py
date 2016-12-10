34a71bed3657bfd4d01648f0e95be2cba2eebc63
Liam Schumm <liamschumm@icloud.com>
2016-12-10 09:10:20 -0600

    Update .travis.yml

--- lib/lib/__init__.py
+++ lib/lib/__init__.py
@@ -2,7 +2,10 @@ import os
 
 verbs = {}
 
-commands = ["bash", "cd", "clear", "cp", "echo", "edit", "ergo_help", "file", "fish", "get", "ls", "mkdir", "mv", "pwd", "python", "quit", "read", "rm", "set", "version", "whoami", "yes", "zsh"]
+commands = ["bash", "cd", "clear", "cp", "echo", "edit", "ergo_help", "file",
+            "fish", "get", "ls", "mkdir", "mv", "pwd", "python", "quit", "read",
+            "rm", "set", "version", "whoami", "yes", "zsh",
+           ]
 
 for item in commands:
     if (item != "__init__.py") and (item[-4:] != ".pyc"):
