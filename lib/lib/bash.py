6d01dd6ef157e383695fe765d01ea8cb6c6f6528
Liam Schumm <liamschumm@icloud.com>
2016-12-18 22:08:17 -0600

    Make bash also share namespace w/ Ergonomica and ptpython

--- lib/lib/bash.py
+++ lib/lib/bash.py
@@ -10,17 +10,19 @@
 Defines the "bash" command.
 """
 
+import os
 from lib.util.util import run_command
 
 verbs = {}
 
 def bash(env, args, kwargs):
     """[STRING,...]@Open a Bash shell. If STRINGs specified, evaluate strings in Bash."""
+    parsed_env = " ".join(["env %s=%s" % (k, env.namespace[k]) for k in env.namespace])
     if args == []:
-        run_command("bash")
+        os.system(parsed_env + " bash")
         #os.system("bash")
     else:
-        return map(run_command, args)
+        return map(run_command, [parsed_env + " " + x for x in args])
     return ""
 
 verbs["bash"] = bash
