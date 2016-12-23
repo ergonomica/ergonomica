c0ef39e356f93dd9fa5cfc65a641ce3970938e02
Liam Schumm <liamschumm@icloud.com>
2016-12-23 13:33:21 -0600

    Add more options to 'size' command

--- lib/lib/size.py
+++ lib/lib/size.py
@@ -17,21 +17,40 @@ from lib.lang.error import ErgonomicaError
 verbs = {}
 
 SIZES = ["byte(s)", "kilobyte(s)", "megabyte(s)", "gigabyte(s)", "terabyte(s)", "petabyte(s)"]
+NAME_SIZES = ["byte", "kilobyte", "megabyte", "gigabyte", "terabyte", "petabyte"]
+SHORT_SIZES = ["B", "kB", "MB", "GB", "TB", "PB"]
 
 def size(env, args, kwargs):
     """[STRING,...]@Prints its input."""
     out = []
-    for item in args:
-        try:
-            size = 0
-            if item[0] in ["/", "~"]:
+    try:
+        unit = kwargs["unit"]
+        size_factor = 1
+        if unit in SHORT_SIZES:
+            size_factor = SHORT_SIZES.index(unit)
+        elif unit in SIZES:
+            size_factor = SIZES.index(unit) 
+        elif unit in NAME_SIZES:
+            size_factor = NAME_SIZES.index(unit)
+                
+        for item in args:
+            try:
                 size = os.path.getsize(item)
-            else:
-                size = os.path.getsize(env.directory + "/" + item)
-                size_factor = int(math.floor(math.log(size) / 6.93147))
-                out.append(item + " : " + str(size / 1024**size_factor) + " " + SIZES[size_factor])
-        except OSError:
-            raise ErgonomicaError("[ergo: NoSuchFileError]: No such file '%s'." % (item))
+                out.append(item + " : " + str(size / 1024 ** size_factor) + " " + SIZES[size_factor])
+            except OSError:
+                raise ErgonomicaError("[ergo: NoSuchFileError]: No such file '%s'." % (item))
+    except KeyError:
+        for item in args:
+            try:
+                size = 0
+                if item[0] in ["/", "~"]:
+                    size = os.path.getsize(item)
+                else:
+                    size = os.path.getsize(env.directory + "/" + item)
+                    size_factor = int(math.floor(math.log(size) / 6.93147))
+                    out.append(item + " : " + str(size / 1024**size_factor) + " " + SIZES[size_factor])
+            except OSError:
+                raise ErgonomicaError("[ergo: NoSuchFileError]: No such file '%s'." % (item))
     return out
 
 verbs["size"] = size
