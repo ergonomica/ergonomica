1aa9ea494bbac7d345040892c8ea6f71e1cf0a6e
Liam Schumm <liamschumm@icloud.com>
2016-12-26 08:16:11 -0500

    Add .inputrc configuration to ergo_setup.sh

--- ergo_setup.sh
+++ ergo_setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 mkdir ~/.ergo && touch ~/.ergo/.ergo_profile && touch ~/.ergo/.ergo_history && mkdir ~/.ergo/packages
+
+read -r -p "Would you like to enable showing autocompletion? (creates a file ~/.initrc) [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+        echo set show-all-if-ambiguous on >>  ~/.inputrc
+        ;;
+    *)
+        ;;
+esac
