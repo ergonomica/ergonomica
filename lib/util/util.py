bb1952f4c94de2d293bdd57cd69926d7fa532447
Liam Schumm <liamschumm@icloud.com>
2016-12-29 19:23:16 -0500

    Make bash running not print as well as outputting

--- lib/util/util.py
+++ lib/util/util.py
@@ -6,6 +6,6 @@ def run_command(env, cmd):
     """given shell command, returns communication tuple of stdout and stderr"""
     try:
         os.environ["PATH"] = env.PATH                                                                                                                                                           
-        return subprocess.Popen(shlex.split(cmd), env=os.environ.copy()).communicate()
+        return subprocess.check_output(shlex.split(cmd), env=os.environ.copy(), stdout=open(os.devnull, 'wb'))
     except subprocess.CalledProcessError:
         return
