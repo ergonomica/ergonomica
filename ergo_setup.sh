f8b346800aa96f2a17389df7e3ef8cf21d9fa653
Liam Schumm <liamschumm@icloud.com>
2017-03-10 14:16:06 -0600

    Make ergo_setup.sh include epm installation option (for easy package management) and remove .initrc creation option (used for deprecated GNU readline)

--- ergo_setup.sh
+++ ergo_setup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-mkdir ~/.ergo && touch ~/.ergo/.ergo_profile && touch ~/.ergo/.ergo_history && mkdir ~/.ergo/packages
+echo "[ergo_setup.sh]: Creating ~/.ergo directory..."
+mkdir ~/.ergo
 
-read -r -p "Would you like to enable showing autocompletion? (creates a file ~/.initrc) [y/N] " response
-case $response in
-    [yY][eE][sS]|[yY]) 
-        echo set show-all-if-ambiguous on >>  ~/.inputrc
-        ;;
-    *)
-        ;;
-esac
+echo "[ergo_setup.sh]: Creating blank ~/.ergo/.ergo_profile and ~/.ergo/.ergo_history files..."
+touch ~/.ergo/.ergo_profile
+touch ~/.ergo/.ergo_history
+
+echo "[ergo_setup.sh]: Creating packages directory in ~/.ergo..."
+mkdir ~/.ergo/packages
+
+read -p "[ergo_setup.sh]: Do you want to install the Ergonomica Package Manager (epm)? (Y\n)" -n 1 -r
+if ! [[ $REPLY =~ ^[Nn]$ ]]
+then
+    echo "[ergo_setup.sh]: Installing epm..."
+    curl -L "https://github.com/ergonomica/package-epm/master/epm.py" > ~/.ergo/packages/epm.py
+    echo "[ergo_setup.sh]: epm successfully installed."
+fi
+
+echo "[ergo_setup.sh]: Setup complete."
