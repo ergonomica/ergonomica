fd696e607f7c6a4f16e2875626b0d95403113a88
Liam Schumm <liamschumm@icloud.com>
2017-03-11 11:40:42 -0600

    Add Ergonomica removal script (for posix systems)

---
+++ ergo_remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "[ergo_remove.sh]: Creating ~/.ergo directory..."
+
+read -p "[ergo_remove.sh]: Are you absolutely sure that you want to delete Ergonomica and all its packages?" -n 1 -r
+if ! [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "[ergo_remove.sh]: Removing ~/.ergo and all its contents..."
+    rm -rf ~/.ergo
+fi
+
+echo "[ergo_remove.sh]: Ergonomica config removal complete."
