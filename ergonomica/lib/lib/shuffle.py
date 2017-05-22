69bad04957bbc639ba20456e1cac7ad54ea93eda
Liam Schumm <liamschumm@icloud.com>
2017-05-22 07:15:31 -0700

    More function fixes; initial fixing of testing framework for Ergonomica red esign

--- ergonomica/lib/lib/shuffle.py
+++ ergonomica/lib/lib/shuffle.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# pylint's name standards are insane
-# pylint: disable=invalid-name
-
-# no other way to do it
-# pylint: disable=line-too-long
-
-# this file is imported from a different directory
-# pylint: disable=import-error
-
-# needed to make the import work
-# pylint: disable=wrong-import-position
-
-# positional arguments are a good standard for commands
-# pylint: disable=unused-argument
-
 """
 [lib/lib/shuffle.py]
 
