25007b4043e390fb48398677ed25cab15c1e82e1
Liam Schumm <liamschumm@icloud.com>
2017-06-01 12:53:39 -0500

    Stdout fixes; fix find function

--- ergonomica/lib/lang/pipe.py
+++ ergonomica/lib/lang/pipe.py
@@ -4,7 +4,7 @@
 """
 [lib/lang/pipe.py]
 
-Piping module.
+The piping module.
 """
 
 import os
@@ -18,43 +18,48 @@ from ergonomica.lib.lang.arguments import get_typed_args
 POOL = Pool(cpu_count())
 
 class Operation(object):
-    def __init__(self, f, args):
-        self.f, self.args = f, args
+    """Defines an Operation object, holding a function and its arguments."""
+    def __init__(self, function, arguments):
+        self.function, self.arguments = function, arguments
 
 class Pipeline(object):
+    """Defines a pipeline object for redirecting the output of some functions to others."""
 
     # tokenized functions to be piped to
     operations = []
     args = []
     env = Environment()
-    ns = {}
+    namespace = {}
 
-    def __init__(self, env, ns):
+    def __init__(self, env, namespace):
+        """Initialize a Pipeline object."""
         self.env = env
-        self.ns = ns
+        self.namespace = namespace
 
-    def append_operation(self, op):
-        self.operations.append(op)
+    def append_operation(self, operation):
+        """Add an Operation type to the end of the Pipeline."""
+        self.operations.append(operation)
 
-    def STDOUT(self):
+    def stdout(self):
+        """Evaluate the entire pipeline, giving the final output."""
         cur = []
         for operation in self.operations:
-            if not callable(operation.f): # then call as shell command
-                os.system("%s %s" % (operation.f, " ".join(operation.args)))
+            if not callable(operation.function): # then call as shell command
+                os.system("%s %s" % (operation.function, " ".join(operation.arguments)))
 
             else:
                 _operation = operation
-                argv = [str(x) for x in _operation.args]
+                argv = [str(x) for x in _operation.arguments]
                 try:
 
                     # it's pretty much impossible to shorten this
                     # pylint: disable=line-too-long
-                    o = lambda x, _operation=_operation: _operation.f(ArgumentsContainer(self.env,
-                                                                                         self.ns,
-                                                                                         x,
-                                                                                         get_typed_args(_operation.f.__doc__, argv)))
-                except DocoptException as e:
-                    return "[ergo: ArgumentError]: %s." % str(e)
+                    o = lambda x, _operation=_operation: _operation.function(ArgumentsContainer(self.env,
+                                                                                                self.namespace,
+                                                                                                x,
+                                                                                                get_typed_args(_operation.function.__doc__, argv)))
+                except DocoptException as error:
+                    return "[ergo: ArgumentError]: %s." % str(error)
                 if cur == []:
                     cur = o(None)
                 else:
@@ -67,7 +72,3 @@ class Pipeline(object):
         self.operations = []
         self.args = []
         return cur
-
-    #stderr
-    def STDERR(self):
-        pass
--- ergonomica/lib/util/setup.py
+++ ergonomica/lib/util/setup.py
@@ -1,6 +1,19 @@
+"""
+[lib/util/setup.py]
+
+The Ergonomica setup script.
+"""
+
 import os
 
+
 def setup():
+    """
+    Set up the users computer for Ergonomica. Note that this is only
+    called when it is known that their computer does not have this structure
+    is installed.
+    """
+
     os.mkdir(os.path.join(os.path.expanduser("~"), ".ergo"))
     os.mkdir(os.path.join(os.path.expanduser("~"), ".ergo", "packages"))
     open(os.path.join(os.path.expanduser("~"), ".ergo", ".ergo_profile"), "w")
