b9c3cd8ba327dfcfecc590d5e91d4c38154a3b23
Liam Schumm <liamschumm@icloud.com>
2017-06-01 19:33:29 -0500

    Lint tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 # -*_ coding: utf-8 -*-
 
+# wildcard imports make testing update dynamically
+# pylint: disable=wildcard-import
+
 """
 [tests/__init__.py]
 
 Ergonomica testing module.
 """
 
-from stdlib import *
-from lang import *
+from tests.stdlib import *
+from tests.lang import *
