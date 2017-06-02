144a804af1e560d17b6cc2f5639cb8bca37dc2fb
Liam Schumm <liamschumm@icloud.com>
2017-06-01 20:17:14 -0500

    Add automatic PyPI deployment through Travis CI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='ergonomica',
-    version='2.0.0-b30',
+    version='2.0.0-b31',
     description='A cross-platform modern shell written in Python.',
     long_description=open('README.rst').read(),
     url='https://ergonomica.github.io/',
