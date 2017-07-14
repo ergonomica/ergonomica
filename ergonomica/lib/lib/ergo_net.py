9dd785199bca3d7744cd0e84c1e8c4efa110ab46
Liam Schumm <liamschumm@icloud.com>
2017-07-13 20:46:18 -0500

    Add interface listing and mac address for net command ; do not print None on commands that return no output

--- ergonomica/lib/lib/ergo_net.py
+++ ergonomica/lib/lib/ergo_net.py
@@ -9,13 +9,16 @@ Defines the "net" command.
 
 import socket
 import requests
+import netifaces
 
 def main(argc):
     """net: Various network information commands.
     Usage:
         net ip (local|global)
+        net mac INTERFACE
+        net interfaces
     """
-    
+
     if argc.args['ip']:
         if argc.args['local']:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,6 +29,11 @@ def main(argc):
         elif argc.args['global']:
             import requests
             yield requests.get('http://ip.42.pl/raw').text
-            
+
+    elif argc.args['mac']:
+        yield netifaces.ifaddresses(argc.args['INTERFACE'])[netifaces.AF_LINK][0]['addr']
+        
+    elif argc.args['interfaces']:
+        yield netifaces.interfaces()
 
    # argc.env.aliases[argc.args['NAME']] = argc.ns[argc.args['FUNCTION']]
