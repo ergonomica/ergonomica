5f109ee81a51385413b8e87f85a7363c610a2476
Liam Schumm <liamschumm@icloud.com>
2017-07-14 08:09:23 -0500

    Automatically escape strings being passed to the system shell

--- ergonomica/ergo.py
+++ ergonomica/ergo.py
@@ -81,6 +81,8 @@ def eval_tokens(*args, **kwargs):
 def raw_eval_tokens(_tokens, namespace, log=False, silent=False):
     """Evaluate Ergonomica tokens."""
 
+    print(_tokens)
+
     new_command = True
     in_function = False
     in_lambda = False
