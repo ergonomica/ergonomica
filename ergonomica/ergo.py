ce262ba876bd9f68c680c212a635925a8fab7238
Liam Schumm <liamschumm@icloud.com>
2017-08-06 08:48:33 -0500

    Remove extra trailing newline on shell commands

--- ergonomica/ergo.py
+++ ergonomica/ergo.py
@@ -250,6 +250,7 @@ def eval(x, ns):
                 p = subprocess.Popen([x[0]] + [eval(i, ns) for i in x[1:]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
                 cur = []
                 for line in iter(p.stdout.readline, ""):
+                    line = line[:-1] # remove the trailing newline
                     print(line)
                     cur.append(line)
                 PRINT_OUTPUT = False
