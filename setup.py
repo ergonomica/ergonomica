ffbb94fed4c079e5f5738ed1f11e2d2b9b5de669
Liam Schumm <liamschumm@icloud.com>
2017-08-07 11:37:46 -0500

    Fix ergo() only printing to STDOUT (not actually returning values)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='ergonomica',
-    version='2.0.2',
+    version='2.0.3',
     description='A cross-platform modern shell written in Python.',
     long_description=open('README.rst').read(),
     url='https://ergonomica.github.io/',
