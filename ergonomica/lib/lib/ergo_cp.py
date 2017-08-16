9b7a36b8d309c8d40558596d0c95e7593c5caa23
Liam Schumm <liamschumm@icloud.com>
2017-08-15 23:53:35 -0500

    Make filter not print False when no matches found; fix copy command; fix broken comment parsing

--- ergonomica/lib/lib/ergo_cp.py
+++ ergonomica/lib/lib/ergo_cp.py
@@ -9,7 +9,15 @@ Defines the "cp" command.
 
 import shutil
 from ergonomica.lib.util.util import expand_path
+import errno
 
+def copyanything(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc: # python >2.5
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else: raise
 
 def cp(argc):
     """cp: Copy files.
@@ -18,7 +26,7 @@ def cp(argc):
         cp SOURCE DESTINATION
     """
 
-    shutil.copy2(expand_path(argc.env, argc.args['SOURCE']),
+    copyanything(expand_path(argc.env, argc.args['SOURCE']),
                  expand_path(argc.env, argc.args['DESTINATION']))
     return
 
