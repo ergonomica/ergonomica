5331914687ad2eebbc2227cb0e50620f4cc35db9
Liam Schumm <liamschumm@icloud.com>
2017-08-18 23:40:32 -0500

    Add link command

---
+++ ergonomica/lib/lib/ergo_link.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+[lib/lib/ergo_link.py]
+
+Defines the "link" command.
+"""
+
+def symlink(source, link_name):
+    import os
+    os_symlink = getattr(os, "symlink", None)
+    if callable(os_symlink):
+        os_symlink(source, link_name)
+    else:
+        import ctypes
+        csl = ctypes.windll.kernel32.CreateSymbolicLinkW
+        csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
+        csl.restype = ctypes.c_ubyte
+        flags = 1 if os.path.isdir(source) else 0
+        if csl(link_name, source, flags) == 0:
+            raise ctypes.WinError()
+
+
+def link(argc):
+    """link: Create a symlink.
+
+    Usage:
+        link SOURCE DESTINATION
+    """
+
+    symlink(argc.args['SOURCE'], argc.args['DESTINATION'])
+
+    return
+
+
+exports = {'link': link}
