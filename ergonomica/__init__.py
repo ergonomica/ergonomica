4f989d8d66db2c93fb808c13ca1b3c1a243d4b27
Liam Schumm <liamschumm@icloud.com>
2017-08-26 22:36:42 -0500

    Allow for `from ergonomica import ergo`

--- ergonomica/__init__.py
+++ ergonomica/__init__.py
@@ -1,2 +1,10 @@
 class ErgonomicaError(Exception):
     pass
+
+def ergo(*argc, **argv):
+    from ergonomica.ergo import ergo
+    return ergo(*argc, **argv)
+    
+def ergo_to_string(*argc, **argv):
+    from ergonomica.ergo import ergo_to_string
+    return ergo_to_string(*argc, **argv)
