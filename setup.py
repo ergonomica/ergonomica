e1221256d889e298b77e42768a735c534bcaa69b
Liam Schumm <liamschumm@icloud.com>
2017-10-02 18:29:12 -0500

    Fix piping when nothing is returned

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 
 setup(
     name='ergonomica',
-    version='2.5.4',
+    version='2.5.5',
     description='A cross-platform modern shell written in Python.',
     long_description=open('README.rst').read(),
     url='https://ergonomica.readthedocs.io',
